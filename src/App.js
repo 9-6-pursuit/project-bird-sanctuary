87602f465203c08fc547b12b1981108b00f0b0de
Lakisha J <lakishajohnson@pursuit.org>
2023-04-30 14:04:36 -0400

    Import useState to get birdData for list of cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-function App () {
+import { useState } from "react";
+import birdData from "./data/birds";
+
+function App() {
+  const [birds, setBirds] = useState(birdData);
+
+  function handleAdoption(id) {
+    setBirds((prevState) =>
+      prevState.map((bird) => {
+        if (bird.id === id) {
+          return { ...bird, adopted: !bird.adopted };
+        }
+        return bird;
+      })
+    );
+  }
+
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <main>
+        <ul className="birds-list">
+          {birds.map((bird) => (
+            <li key={bird.id} className="bird-card">
+              <h2>{bird.name}</h2>
+              <p>Price: ${bird.amount}</p>
+              <img src={bird.img} alt={bird.name} />
+              <button onClick={() => handleAdoption(bird.id)}>
+                {" "}
+                {bird.adopted ? "Adopted" : "Adopt"}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
-};
+}
 
 export default App;
