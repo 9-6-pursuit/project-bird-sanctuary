6deb03549ecb2fd2b7e5d05b6be540ef634ebab3
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-30 14:32:28 -0400

    checkout form

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -5,6 +5,31 @@ function Checkout() {
     <>
     <div className="checkout">
     <h4>Checkout</h4>
+    <form>
+        <label>First Name</label>
+        <input
+        type="text"
+        id="first-name"
+        />
+        <label>Last Name</label>
+        <input
+        type="text"
+        id="last-name"
+        />
+        <label>Email</label>
+        <input
+        type="text"
+        id="email"
+        />
+        <label>Zip Code</label>
+        <input
+        type="text"
+        id="email"
+        />
+
+        <button>Submit</button>
+
+    </form>
     </div>
     </>
   );
