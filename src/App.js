1ec5f15eed32c51226a858f33679a76cd5b70c93
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-05-01 14:04:02 -0400

    I'm trying to make the images appear

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import birdData from "./data/birds"
+import { useState } from 'react'
+
+
+
+
 function App () {
+  const [birds, setBirds] = useState(birdData)
+  console.log(birdData)
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="App">
+     <h1>Bird Sanctuary Project</h1>
+
+    <ul>
+      {
+        birds.map((bird) => {
+          return (
+            <li key = {bird.id}>
+              <span>{bird.img}</span>
+              <span>{bird.name}</span>
+              <span> - ${bird.amount}</span>
+              <button>Adopt Me</button>
+            </li>
+          )
+        })
+
+      }
+    </ul>
+
+
     </div>
+
   );
 };
 
