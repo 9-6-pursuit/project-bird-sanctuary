cd3bdb751062ab172c823416e27bb5ba1ae961fd
Lakisha J <lakishajohnson@pursuit.org>
2023-05-01 21:20:59 -0400

    Added bonuses

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,23 @@
+import React, { useState, useEffect } from "react";
+import bonusItems from "../data/bonusItems";
+
 function Cart({ selectedBirds, totalPrice, discount }) {
-  
+  const [bonuses, setBonuses] = useState([]);
+
+  useEffect(() => {
+    let total = selectedBirds.reduce((acc, bird) => acc + bird.amount, 0);
+    let bonusCount = 0;
+    if (total >= 100 && total < 300) {
+      bonusCount = 1;
+    } else if (total >= 300 && total < 500) {
+      bonusCount = 2;
+    } else if (total >= 500 && total < 1000) {
+      bonusCount = 3;
+    } else if (total >= 1000) {
+      bonusCount = 4;
+    }
+    setBonuses(bonusItems.slice(0, bonusCount));
+  }, [selectedBirds]);
 
   return (
     <div className="cart-container">
@@ -14,6 +32,16 @@ function Cart({ selectedBirds, totalPrice, discount }) {
             </li>
           ))}
         </ol>
+        {bonuses.length > 0 && (
+          <>
+            <p><strong>Your donations have qualified you for the following items:</strong></p>
+            <ul>
+              {bonuses.map((bonus) => (
+                <li key={bonus}>{bonus}</li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </div>
   );
