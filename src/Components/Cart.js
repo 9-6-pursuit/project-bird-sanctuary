ba4ac24dcdae4d5cf23a3b69ac26381c69ef8c5c
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-05-01 23:32:04 -0400

    bonus working

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -10,9 +10,23 @@ export default function Cart({
   const totalCost = calculateTotalCost();
   const discount = cartItems.length >= 3 ? 0.1 : 0;
   const discountedCost = applyDiscount(totalCost, discount);
-  const bonusItems = bonusItemsData.filter(
-    (bonusItem) => discountedCost >= bonusItem.threshold
-  );
+
+  let bonusCount = 0;
+  let bonusIndex = 0;
+  if (discountedCost >= 100 && discountedCost < 300) {
+    bonusCount = 1;
+    bonusIndex = 0;
+  } else if (discountedCost >= 300 && discountedCost < 500) {
+    bonusCount = 2;
+    bonusIndex = 1;
+  } else if (discountedCost >= 500 && discountedCost < 1000) {
+    bonusCount = 3;
+    bonusIndex = 2;
+  } else if (discountedCost >= 1000) {
+    bonusCount = 4;
+    bonusIndex = 3;
+  }
+  const bonusItems = bonusItemsData.slice(bonusIndex, bonusIndex + bonusCount);
 
   return (
     <div className="Cart">
@@ -33,7 +47,7 @@ export default function Cart({
           <h4>Bonus Items:</h4>
           <ul>
             {bonusItems.map((bonusItem) => (
-              <li key={bonusItem.id}>{bonusItem.name}</li>
+              <li key={bonusItem}>{bonusItem}</li>
             ))}
           </ul>
         </div>
