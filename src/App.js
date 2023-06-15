eda53619e1810efee1b92af632e1827aab047003
winterbelle <blancaaltamirano@pursuit.org>
2023-06-15 02:25:18 -0400

    Added Components and imported them to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+// import { useState } from "react";
+// import birdData from "./data/birds.js";
+// import {bonusItems} from "./data/bonusItems.js";
+import BirdCards from "./Components/BirdCards";
+import Carts from "./Components/Cart.js";
+import Checkout from "./Components/Checkout";
+
 function App () {
+  // const [birds, setBirds] = useState(birdData)
+
+  // function birdNames(params) {
+  //   setBirds(params)
+  // }
+
+
   return (
     <div>
       <h1>Hello, world!</h1>
+      <BirdCards/>
+      <Carts/>
+      <Checkout/>
+      {/* <bonusItems/> */}
     </div>
   );
 };
