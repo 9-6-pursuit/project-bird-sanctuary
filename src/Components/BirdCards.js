4718dafd9be9012f3062880841904039c66d86fc
winterbelle <blancaaltamirano@pursuit.org>
2023-06-15 03:09:27 -0400

    project passes all test

--- src/Components/BirdCards.js
+++ src/Components/BirdCards.js
@@ -1,17 +1,19 @@
 export default function BirdCards({birds, adopt}) {
     return (
-        <div className = "row" id="Birds">
-          {birds.map(bird => (
-            <div key={bird.id} className="col-sm-4 col-md-4 col-lg-4 card"> 
-                <h3 className="birds">{bird.name}</h3>
-                <h3>{bird.amount}</h3>
-                <img className="poster"src={bird.img} alt={bird.name}></img><br/>
-                <button  onClick={() =>adopt(bird.id)}>
-                Adopt
-                </button>
-                <br />
+        <div className = "card" id="Birds">
+            <div className="row">
+            {birds.map(bird => (
+                <div key={bird.id} className="col-sm-4 col-md-4 col-lg-4 birds"> 
+                    <h3 >{bird.name}</h3>
+                    <h3>{bird.amount}</h3>
+                    <img className="poster"src={bird.img} alt={bird.name}></img><br/>
+                    <button  onClick={() =>adopt(bird.id)}>
+                    Adopt
+                    </button>
+                    <br />
+                </div>
+            ))}
             </div>
-          ))}
         </div>
     );
 }
